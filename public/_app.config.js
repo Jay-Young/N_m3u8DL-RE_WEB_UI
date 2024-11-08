4ef86af5841693b5991e6397aef91f21929627af
aidenconst <aiden_xie@qq.com>
2024-11-08 11:30:47 +0800

    更新 _app.config.js

---
+++ public/_app.config.js
@@ -0,0 +1,1 @@
+window._VBEN_ADMIN_PRO_APP_CONF_={"VITE_GLOB_API_URL":"http://localhost:3600/","VITE_GLOB_WEB_TITLE":"m3u8DL_console"};Object.freeze(window._VBEN_ADMIN_PRO_APP_CONF_);Object.defineProperty(window,"_VBEN_ADMIN_PRO_APP_CONF_",{configurable:false,writable:false,});

89d0e065c51a58dcdf0e1ce91a96ac656d53852a
aidenconst <aiden_xie@qq.com>
2024-11-08 11:29:46 +0800

    更新 _app.config.js

--- public/_app.config.js
+++
@@ -1,1 +0,0 @@
-window._VBEN_ADMIN_PRO_APP_CONF_={"VITE_GLOB_API_URL":"http://mosang.asia:3600/","VITE_GLOB_WEB_TITLE":"m3u8DL_console"};Object.freeze(window._VBEN_ADMIN_PRO_APP_CONF_);Object.defineProperty(window,"_VBEN_ADMIN_PRO_APP_CONF_",{configurable:false,writable:false,});

8a7ac192e2fbc35bb7be7a45e6beb38bc0e788f4
aidenconst <aiden_xie@qq.com>
2024-11-10 20:47:14 +0800

    新增自动配置端口号

--- public/_app.config.js
+++ public/_app.config.js
@@ -1,1 +1,5 @@
-window._VBEN_ADMIN_PRO_APP_CONF_={"VITE_GLOB_API_URL":"http://localhost:3600/","VITE_GLOB_WEB_TITLE":"m3u8DL_console"};Object.freeze(window._VBEN_ADMIN_PRO_APP_CONF_);Object.defineProperty(window,"_VBEN_ADMIN_PRO_APP_CONF_",{configurable:false,writable:false,});
+var protocol = window.location.protocol;
+var hostname = window.location.hostname;
+var port = window.location.port ? ':' + window.location.port : ''; // 如果端口号不是默认，则添加':'和端口号
+var baseUrl = protocol + '//' + hostname + port;
+window._VBEN_ADMIN_PRO_APP_CONF_={"VITE_GLOB_API_URL":baseUrl,"VITE_GLOB_WEB_TITLE":"m3u8DL_console"};Object.freeze(window._VBEN_ADMIN_PRO_APP_CONF_);Object.defineProperty(window,"_VBEN_ADMIN_PRO_APP_CONF_",{configurable:false,writable:false,});
